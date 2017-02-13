ccff64a8a251f74c45e2300245b9df06e08641b2
Nver Abgaryan <abgaryannver1993@gmail.com>
2017-02-13 18:47:54 +0400

    [FEAT](cli)  Add port CLI argument
    Examples:
    npm start -- p 3000
    node app.js p 3000

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ const views = require('co-views');
 const koa = require('koa');
 const path = require('path');
 const app = koa();
-const promisify = require("promisify-node")
-const fse = promisify(require("fs-extra"))
-const fs = require('fs')
+const promisify = require("promisify-node");
+const fse = promisify(require("fs-extra"));
+const fs = require('fs');
 const serve = require('koa-static');
 const bodyParser = require('koa-bodyparser');
 
+const args = process.argv;
+let port = 8000;
+
 app.use(logger());
-app.use(bodyParser())
+app.use(bodyParser());
 app.use(serve(__dirname + '/dist'));
 
 const render = views(path.join(__dirname, '/views'), {ext: 'ejs'});
@@ -19,41 +22,40 @@ const render = views(path.join(__dirname, '/views'), {ext: 'ejs'});
 router.get(
     '/',
     function *(next) {
-        var streamUrl = JSON.parse(yield fse.readFile(path.join(__dirname, 'json/url.json'), 'utf8'))
-        console.log(streamUrl)
+        var streamUrl = JSON.parse(yield fse.readFile(path.join(__dirname, 'json/url.json'), 'utf8'));
+        console.log(streamUrl);
         this.body = yield render('user', {user: streamUrl});
     }
-)
+);
 
 router.post(
     '/url',
     function *(next) {
-        console.log(this.request.body)
-        var streamUrl = JSON.parse(yield fse.readFile(path.join(__dirname, 'json/url.json'), 'utf8'))
-        console.log(streamUrl)
+        console.log(this.request.body);
+        var streamUrl = JSON.parse(yield fse.readFile(path.join(__dirname, 'json/url.json'), 'utf8'));
+        console.log(streamUrl);
 
-        this.body = streamUrl
+        this.body = streamUrl;
     }
-)
+);
 
-router.get(
-    '/mobile',
-    function *(next) {
-        var streamUrl = JSON.parse(yield fse.readFile(path.join(__dirname, 'json/url.json'), 'utf8'))
-        this.body = streamUrl
-    }
-)
-
-app.use(router.routes())
+app.use(router.routes());
 app.use(router.allowedMethods());
 
 // catch all middleware, only land here
 // if no other routing rules match
 // make sure it is added after everything else
 app.use(function *() {
-    this.body = 'Invalid URL!!!';
+    this.status = 404;
+    this.body = 'Invalid URL!';
     // or redirect etc
     // this.redirect('/someotherspot');
 });
 
-app.listen(8000,"0.0.0.0");
+args.forEach((x, y)=>{
+    if (x == "p") {
+        port = args[y + 1];
+    }
+});
+
+app.listen(port, "127.0.0.1");
