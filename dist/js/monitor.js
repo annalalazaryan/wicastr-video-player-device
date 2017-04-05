fe4642c0cfc79e107ef33f2be3b06268a5c5f6ec
Felix Khachatrian <felix.khachatrian@gmail.com>
2017-04-05 19:05:56 +0400

    move monitor to a class, reload page on connection restore

--- dist/js/monitor.js
+++ dist/js/monitor.js
@@ -1,33 +1,67 @@
 $(document).ready(function () {
-    var monitorURL = "http://monitor.wicastr.in";
-    var monitorDelay = 20000;
-    var monitorRemoveDelay = 60000;
-    var currSysState;
+    var Monitor = function() {
+        var self = this;
+        this.currSysState;
+        this.monitorRemoveTimeout;
 
-    var requestSysInfo = function() {
-        $.get(monitorURL+'/sysinfo', function (data) {
-            setupMonitorIframe(data.info);
+        this.settings = {
+            "url": "http://monitor.wicastr.in",
+            "monitorDelay": 20000,
+            "monitorRemoveDelay": 60000
+        }
 
-            currSysState = data.info;
-        });
+        this.requestSysInfo = function() {
+            $.get(self.settings.url + '/sysinfo', function (data) {
+                self.setupMonitorIframe(data.info);
 
-        setTimeout(requestSysInfo, monitorDelay);
-    };
+                self.refreshDigitalSignage(data.info);
 
-    var setupMonitorIframe = function(sysinfo) {
-        var connected = (sysinfo.inet.status == "success");
+                self.currSysState = data.info;
+            });
 
-        if (currSysState && connected) {
-            setTimeout(function(){
+            setTimeout(self.requestSysInfo, self.settings.monitorDelay);
+        };
+
+        this.setupMonitorIframe = function(sysinfo) {
+            var connected = (sysinfo.inet.status == "success");
+
+            if (self.currSysState && connected) {
+                self.removeIframeWithDelay();
+            } else {
+
+                self.cancelIframeRemoval();
+
+                if (!$("#monitor-iframe").length) {
+                    iframe = $('<iframe id="monitor-iframe" frameBorder="0" allowtransparency="true" src="' + self.settings.url + '"></iframe>');
+                    $("#monitor-overlay").html(iframe);
+                }
+
+                $("#monitor-iframe").show();
+            }
+        };
+
+        this.refreshDigitalSignage = function(data) {
+            var connectionRestored = self.currSysState && 
+                self.currSysState.inet.status == "danger" && 
+                data.inet.status == "success";
+
+            if (connectionRestored) {
+                location.reload();
+            }
+        };
+
+        this.removeIframeWithDelay = function() {
+            self.monitorRemoveTimeout = setTimeout(function(){
                 $("#monitor-iframe").remove();
-            }, monitorRemoveDelay); 
-        } else {
-            iframe = $('<iframe id="monitor-iframe" frameBorder="0" allowtransparency="true" src="' + monitorURL + '"></iframe>');
-            $("#monitor-overlay").html(iframe);
-            $("#monitor-iframe").show();
+            }, self.settings.monitorRemoveDelay);
+        };
+
+        this.cancelIframeRemoval = function() {
+            clearTimeout(self.monitorRemoveTimeout);
         }
     };
 
-    requestSysInfo();
+    var monitorInst = new Monitor();
+    monitorInst.requestSysInfo();
 });
 
