c3042add86c49fb8de784469062ac993f602fd1f
Felix Khachatrian <felix.khachatrian@gmail.com>
2017-06-15 15:54:59 +0400

    re-enable playlist re-enable life and dreams

--- dist/js/video.js
+++ dist/js/video.js
@@ -167,7 +167,7 @@ $(document).ready(function () {
         };
 
         this.setup = function () {
-            //this.requestPlaylistWithDelay();
+            this.requestPlaylistWithDelay();
             
             this.createVideo();
 
@@ -207,7 +207,8 @@ $(document).ready(function () {
         };
 
         this.browser = function () {
-            var url = "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8";
+            // var url = "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8";
+            var url = this.settings.videoItemsVideo[this.settings.num].url;
 
             this.playVideo(url);
         };
